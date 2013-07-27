995348f0ba9652e98af7684c5f072c73ded36d09
Sebastian Schmidt <yath@yath.de>
2013-07-27 11:02:20 +0200

    GPLv2 - without the 'or any later version'

--- hotspot.sh
+++ hotspot.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+# hotspot - Script for registering into a T-Mobile hotspot
+# Copyright (C) 2013  Sebastian Schmidt <yath@yath.de>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation - version 2 and no different or later
+# version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 set -e
 
 CONFIG=/etc/hotspot.conf
